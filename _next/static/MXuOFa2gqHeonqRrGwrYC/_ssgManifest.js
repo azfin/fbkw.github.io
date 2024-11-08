d8afaabeef49e8919b112a6d4d4dece44cd0a0db
thang <thanglhssi@gmail.com>
2024-11-08 23:26:28 +0700

    Deploy

---
+++ _next/static/MXuOFa2gqHeonqRrGwrYC/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fdu-lieu-nganh","\u002Fdu-lieu-ttck","\u002Fuser-waiting-list","\u002Fget-golds","\u002Fuser-approved-list","\u002Fazu-inf","\u002Fdanh-muc-dau-tu-config","\u002Fdang-ky-san-pham","\u002Fdanh-muc-dau-tu"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
